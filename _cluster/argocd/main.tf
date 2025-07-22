c115e4c5b63f7bc17a6f50516e3527f1df5a55f6
Joao Leal <legitlealj@gmail.com>
2025-07-22 00:19:03 -0300

    update homelab

---
+++ _cluster/argocd/main.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "8.1.4"
+  timeout          = 600
+  values           = [file("template/values.yaml")]
+}
---
+++ _cluster/metallb/main.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "metallb" {
+  name             = "metallb"
+  repository       = "https://metallb.github.io/metallb"
+  chart            = "metallb"
+  version          = "0.13.10"
+  namespace        = "metallb"
+  create_namespace = true
+}
